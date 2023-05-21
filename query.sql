80110561a6863d583a9a4313b173828cff4f6f6a
Antonella Bevilacqua <abevilacqua@frba.utn.edu.ar>
2023-05-21 13:30:01 -0300

    Cambios en PKs, elimino tablas estados intermedias + store procedures

--- query.sql
+++ query.sql
@@ -1,12 +1,10 @@
 USE GD1C2023
 GO
 
-/*son 34 drops*/
+/*son 31 drops*/
 /*********************** Limpiar tablas ***********************/
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'estado_mensajeria')
 DROP TABLE MargeCreoQueOdioGDD.estado_mensajeria
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'estadoxmensajeria')
-DROP TABLE MargeCreoQueOdioGDD.estadoxmensajeria
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'envio_mensajeria')
 DROP TABLE MargeCreoQueOdioGDD.envio_mensajeria
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'paquete')
@@ -41,8 +39,6 @@ IF EXISTS (SELECT name FROM sys.tables WHERE name = 'producto')
 DROP TABLE MargeCreoQueOdioGDD.producto
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'productoxpedido')
 DROP TABLE MargeCreoQueOdioGDD.productoxpedido
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'estadoxpedido')
-DROP TABLE MargeCreoQueOdioGDD.estadoxpedido
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'estado_pedido')
 DROP TABLE MargeCreoQueOdioGDD.estado_pedido
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'descuentoxpedido')
@@ -61,8 +57,6 @@ IF EXISTS (SELECT name FROM sys.tables WHERE name = 'reclamo')
 DROP TABLE MargeCreoQueOdioGDD.reclamo
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'tipo_reclamo')
 DROP TABLE MargeCreoQueOdioGDD.tipo_reclamo
-IF EXISTS (SELECT name FROM sys.tables WHERE name = 'estadoxreclamo')
-DROP TABLE MargeCreoQueOdioGDD.estadoxreclamo
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'estado_reclamo')
 DROP TABLE MargeCreoQueOdioGDD.estado_reclamo
 IF EXISTS (SELECT name FROM sys.tables WHERE name = 'operador')
@@ -74,7 +68,9 @@ DROP TABLE MargeCreoQueOdioGDD.tipo_medio_pago
 
 /*********************** Limpiar Schema ***********************/
 IF EXISTS (SELECT name FROM sys.schemas WHERE name = 'MargeCreoQueOdioGDD')
-DROP SCHEMA MargeCreoQueOdioGDD
+BEGIN
+	DROP SCHEMA MargeCreoQueOdioGDD
+END
 GO
 
 /*********************** Creacion de Schema ***********************/
@@ -142,27 +138,26 @@ PRIMARY KEY (ID_DIA)
 
 -------------------------- PEDIDO --------------------------
 CREATE TABLE MargeCreoQueOdioGDD.pedido (
-NRO_PEDIDO INT IDENTITY(1,1), --PK
-FECHA_HORA_PEDIDO DATETIME,
-NOMBRE NVARCHAR(255),
-ID_USUARIO INT NOT NULL, --FK
+ID_PEDIDO INT IDENTITY(1,1), --PK
+NRO_PEDIDO INT NOT NULL, --PK
+FECHA_HORA_PEDIDO DATETIME NOT NULL,
 FECHA_HORA_ENTREGA DATETIME,
-ID_DIRECCION_ORIGEN INT NOT NULL, --FK
+ID_USUARIO INT NOT NULL, --FK
 ID_LOCAL INT NOT NULL, --FK
 ID_MEDIO_DE_PAGO INT NOT NULL, --FK
 ID_ENVIO INT NOT NULL, --FK
-FECHA_HORA DATETIME,
-TARIFA_SERVICIO FLOAT,
-TOTAL_PRODUCTOS FLOAT,
-TOTAL_CUPONES FLOAT,
-TOTAL_SERVICIO FLOAT,
+ID_ESTADO INT NOT NULL, --FK
+TARIFA_SERVICIO FLOAT NOT NULL,
+TOTAL_PRODUCTOS FLOAT NOT NULL,
+TOTAL_CUPONES FLOAT NOT NULL,
+TOTAL_SERVICIO FLOAT NOT NULL,
+TOTAL_PEDIDO FLOAT NOT NULL,
 CALIFICACION DECIMAL(5,1),
 OBSERVACIONES NVARCHAR(255),
-TOTAL_PEDIDO FLOAT,
-PRIMARY KEY (NRO_PEDIDO)
+PRIMARY KEY (ID_PEDIDO, NRO_PEDIDO)
 );
 
-CREATE TABLE MargeCreoQueOdioGDD.prodcutoxpedido (
+CREATE TABLE MargeCreoQueOdioGDD.productoxpedido (
 ID_PEDIDO INT NOT NULL, --FK
 ID_PRODUCTO INT NOT NULL, --FK
 CANTIDAD INT,
@@ -178,11 +173,6 @@ PRECIO FLOAT,
 PRIMARY KEY (CODIGO_PRODUCTO)
 );
 
-CREATE TABLE MargeCreoQueOdioGDD.estadoxpedido (
-ID_PEDIDO INT NOT NULL, --FK
-ID_ESTADO INT NOT NULL, --FK
-);
-
 CREATE TABLE MargeCreoQueOdioGDD.estado_pedido(
 ID_ESTADO INT IDENTITY(1,1), --PK
 NOMBRE NVARCHAR(255) NOT NULL,
@@ -191,12 +181,14 @@ PRIMARY KEY (ID_ESTADO)
 
 -------------------------- CUPON DE DESCUENTO --------------------------
 CREATE TABLE MargeCreoQueOdioGDD.cupon_descuento (
-CODIGO INT IDENTITY(1,1), --PK
+--ID_CUPON INT IDENTITY(1,1), --PK
+CODIGO INT NOT NULL, --PK
 ID_USUARIO INT NOT NULL, --FK
 MONTO FLOAT,
 ID_TIPO_CUPON INT NOT NULL, --FK
 FECHA_ALTA DATETIME,
 FECHA_VENCIMIENTO DATETIME,
+--PRIMARY KEY (ID_CUPON, CODIGO)
 PRIMARY KEY (CODIGO)
 );
 
@@ -269,9 +261,9 @@ CREATE TABLE MargeCreoQueOdioGDD.direccion (
 ID_DIRECCION INT IDENTITY(1,1), --PK
 ID_LOCALIDAD INT NOT NULL, --FK
 NOMBRE_DIRECCION NVARCHAR(255),
-CALLE NVARCHAR(255),
-NUMERO INT,
-CODIGO_POSTAL NVARCHAR(255),
+CALLE NVARCHAR(255) NOT NULL,
+NUMERO INT NOT NULL,
+CODIGO_POSTAL NVARCHAR(255) NOT NULL,
 DEPTO NVARCHAR(255),
 OBSERVACIONES NVARCHAR(255),
 PRIMARY KEY (ID_DIRECCION)
@@ -279,51 +271,48 @@ PRIMARY KEY (ID_DIRECCION)
 
 CREATE TABLE MargeCreoQueOdioGDD.localidad (
 ID_LOCALIDAD INT IDENTITY(1,1), --PK
-NOMBRE NVARCHAR(255),
+NOMBRE NVARCHAR(255) NOT NULL,
 ID_PROVINCIA INT NOT NULL, --FK
 PRIMARY KEY (ID_LOCALIDAD)
 );
 
 CREATE TABLE MargeCreoQueOdioGDD.provincia (
 ID_PROVINCIA INT IDENTITY(1,1), --PK
-NOMBRE NVARCHAR(255),
+NOMBRE NVARCHAR(255) NOT NULL,
 PRIMARY KEY (ID_PROVINCIA)
 );
 
 -------------------------- ENVIO MENSAJERIA --------------------------
-CREATE TABLE MargeCreoQueOdioGDD.estado_mensajeria(
-ID_ESTADO INT IDENTITY(1,1), --PK /*identity hace que se autoincremente*/
-NOMBRE NVARCHAR(255) NOT NULL,
-PRIMARY KEY (ID_ESTADO)
-);
-
 CREATE TABLE MargeCreoQueOdioGDD.envio_mensajeria(
-NRO_ENVIO_MENSAJERIA INT IDENTITY(1,1), --PK
-NOMBRE NVARCHAR(255) NOT NULL,
+ID_ENVIO_MENSAJERIA INT IDENTITY(1,1), --PK
+NRO_ENVIO_MENSAJERIA INT NOT NULL, --PK
 ID_USUARIO INT NOT NULL, --FK
 ID_DIRECCION_ORIGEN INT NOT NULL, --FK
 ID_MEDIO_PAGO INT NOT NULL, --FK
 ID_PAQUETE INT NOT NULL, --FK
 ID_ENVIO INT NOT NULL, --FK
+ID_ESTADO INT NOT NULL, --FK
 FECHA_HORA_PEDIDO DATETIME,
 FECHA_HORA_ENTREGA DATETIME,
-DISTANCIA_KMS INT,
+DISTANCIA_KMS INT NOT NULL,
 CALIFICACION DECIMAL(5,1),
 OBSERVACIONES NVARCHAR(255),
-PRECIO_POR_SEGURO FLOAT,
-TOTAL_SERVICIO_MENSAJERIA FLOAT,
-PRIMARY KEY (NRO_ENVIO_MENSAJERIA)
+PRECIO_POR_SEGURO FLOAT NOT NULL,
+TOTAL_SERVICIO_MENSAJERIA FLOAT NOT NULL,
+PRIMARY KEY (ID_ENVIO_MENSAJERIA, NRO_ENVIO_MENSAJERIA)
 );
 
-CREATE TABLE MargeCreoQueOdioGDD.estadoxmensajeria (
-ID_MENSAJERIA INT NOT NULL, --FK
-ID_ESTADO INT NOT NULL, --FK
+CREATE TABLE MargeCreoQueOdioGDD.estado_mensajeria(
+ID_ESTADO INT IDENTITY(1,1), --PK
+NOMBRE NVARCHAR(255) NOT NULL,
+PRIMARY KEY (ID_ESTADO)
 );
 
 CREATE TABLE MargeCreoQueOdioGDD.paquete (
 ID_PAQUETE INT IDENTITY(1,1), --PK
 ID_TIPO_PAQUETE INT NOT NULL, --FK
-VALOR_ASEGURADO FLOAT
+VALOR_ASEGURADO FLOAT,
+PRIMARY KEY(ID_PAQUETE)
 );
 
 CREATE TABLE MargeCreoQueOdioGDD.tipo_paquete (
@@ -332,33 +321,31 @@ TIPO_PAQUETE NVARCHAR(255),
 MEDIDA_ANCHO INT,
 MEDIDA_LARGO INT,
 MEDIDA_ALTO INT,
-PESO_MAXIMO FLOAT
+PESO_MAXIMO FLOAT,
+PRIMARY KEY(ID_TIPO_PAQUETE)
 );
 
 -------------------------- RECLAMO --------------------------
 CREATE TABLE MargeCreoQueOdioGDD.reclamo (
-NRO_RECLAMO INT IDENTITY(1,1), --PK
+ID_RECLAMO INT IDENTITY(1,1), --PK
+NRO_RECLAMO INT NOT NULL, --PK
 ID_USUARIO INT NOT NULL, --FK
 ID_PEDIDO INT NOT NULL, --FK
 ID_OPERADOR INT NOT NULL, --FK
+ID_ESTADO INT NOT NULL, --FK
 DESCRIPCION NVARCHAR(255),
 SOLUCION NVARCHAR(255),
 ID_TIPO_RECLAMO INT NOT NULL, --FK
 CALIFICACION DECIMAL(5,1),
 FECHA_HORA_INICIO DATETIME,
 FECHA_HORA_SOLUCION DATETIME,
-PRIMARY KEY (NRO_RECLAMO)
+PRIMARY KEY (ID_RECLAMO, NRO_RECLAMO)
 );
 
 CREATE TABLE MargeCreoQueOdioGDD.tipo_reclamo (
 ID_TIPO_RECLAMO INT IDENTITY(1,1), --PK
 TIPO_RECLAMO NVARCHAR(255),
 PRIMARY KEY (ID_TIPO_RECLAMO)
-);
-
-CREATE TABLE MargeCreoQueOdioGDD.estadoxreclamo (
-ID_ESTADO INT NOT NULL, --FK
-ID_RECLAMO INT NOT NULL, --FK
 );
 
 CREATE TABLE MargeCreoQueOdioGDD.estado_reclamo (
@@ -387,20 +374,19 @@ ADD CONSTRAINT FK_DIRECCION_USUARIO
 FOREIGN KEY (ID_DIRECCION) REFERENCES MargeCreoQueOdioGDD.direccion
 
 -------------------------- LOCAL --------------------------
--------------------------- local --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.local
 ADD CONSTRAINT FK_DIRECCION_LOCAL
 FOREIGN KEY (ID_DIRECCION) REFERENCES MargeCreoQueOdioGDD.direccion
 
 ALTER TABLE MargeCreoQueOdioGDD.local
 ADD CONSTRAINT FK_CATEGORIA_LOCAL
-FOREIGN KEY (ID_CATEGORIA) REFERENCES MargeCreoQueOdioGDD.categoria
+FOREIGN KEY (ID_CATEGORIA) REFERENCES MargeCreoQueOdioGDD.categoria_local
 
 ALTER TABLE MargeCreoQueOdioGDD.categoria_local
 ADD CONSTRAINT FK_TIPO_CATEGORIA_LOCAL
 FOREIGN KEY (ID_TIPO) REFERENCES MargeCreoQueOdioGDD.tipo_local
 
--------------------------- horario local --------------------------
+-------------------------- HORARIO LOCAL --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.horario_local
 ADD CONSTRAINT FK_LOCAL_HORARIO_LOCAL
 FOREIGN KEY (ID_LOCAL) REFERENCES MargeCreoQueOdioGDD.local
@@ -414,14 +400,13 @@ ADD CONSTRAINT FK_HORARIO_HORARIOXDIA
 FOREIGN KEY (ID_HORARIO_LOCAL) REFERENCES MargeCreoQueOdioGDD.horario_local
 
 -------------------------- PEDIDO --------------------------
--------------------------- pedido --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.pedido
 ADD CONSTRAINT FK_USUARIO_PEDIDO
 FOREIGN KEY (ID_USUARIO) REFERENCES MargeCreoQueOdioGDD.usuario
 
 ALTER TABLE MargeCreoQueOdioGDD.pedido
-ADD CONSTRAINT FK_DIRECCION_ORIGEN_PEDIDO
-FOREIGN KEY (ID_DIRECCION_ORIGEN) REFERENCES MargeCreoQueOdioGDD.direccion
+ADD CONSTRAINT FK_ESTADO_PEDIDO
+FOREIGN KEY (ID_ESTADO) REFERENCES MargeCreoQueOdioGDD.estado_pedido
 
 ALTER TABLE MargeCreoQueOdioGDD.pedido
 ADD CONSTRAINT FK_LOCAL_PEDIDO
@@ -435,25 +420,16 @@ ALTER TABLE MargeCreoQueOdioGDD.pedido
 ADD CONSTRAINT FK_ENVIO_PEDIDO
 FOREIGN KEY (ID_ENVIO) REFERENCES MargeCreoQueOdioGDD.envio
 
--------------------------- estadoxpedido --------------------------
-ALTER TABLE MargeCreoQueOdioGDD.estadoxpedido
-ADD CONSTRAINT FK_PEDIDO_ESTADOXPEDIDO 
-FOREIGN KEY (ID_PEDIDO) REFERENCES MargeCreoQueOdioGDD.pedido
-
-ALTER TABLE MargeCreoQueOdioGDD.estadoxpedido
-ADD CONSTRAINT FK_ESTADO_ESTADOXPEDIDO 
-FOREIGN KEY (ID_ESTADO) REFERENCES MargeCreoQueOdioGDD.estado_pedido
-
--------------------------- productoxpedido --------------------------
+-------------------------- ProductoXPedido --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.productoxpedido
 ADD CONSTRAINT FK_PEDIDO_PRODUCTOXPEDIDO 
-FOREIGN KEY (ID_PEDIDO) REFERENCES MargeCreoQueOdioGDD.pedido
+FOREIGN KEY (ID_PEDIDO) REFERENCES MargeCreoQueOdioGDD.pedido --TODO: FALLA PEDIDO AHORA TIENE UNA PK DE 2 CAMPOS, COMO SE GENERA LA FK?
 
 ALTER TABLE MargeCreoQueOdioGDD.productoxpedido
 ADD CONSTRAINT FK_PRODUCTO_PRODUCTOXPEDIDO 
 FOREIGN KEY (ID_PRODUCTO) REFERENCES MargeCreoQueOdioGDD.producto
 
--------------------------- producto --------------------------
+-------------------------- PRODUCTO --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.producto
 ADD CONSTRAINT FK_LOCAL_PRODUCTO
 FOREIGN KEY (ID_LOCAL) REFERENCES MargeCreoQueOdioGDD.local
@@ -469,7 +445,7 @@ FOREIGN KEY (ID_TIPO_CUPON) REFERENCES MargeCreoQueOdioGDD.tipo_cupon
 
 ALTER TABLE MargeCreoQueOdioGDD.descuentoxreclamo
 ADD CONSTRAINT FK_RECLAMO_DESCUENTOXRECLAMO
-FOREIGN KEY (ID_RECLAMO) REFERENCES MargeCreoQueOdioGDD.reclamo
+FOREIGN KEY (ID_RECLAMO) REFERENCES MargeCreoQueOdioGDD.reclamo --TODO: IDEM PEDIDO, AHORA RECLAMO TIENE UNA PK DE 2 CAMPOS
 
 ALTER TABLE MargeCreoQueOdioGDD.descuentoxreclamo
 ADD CONSTRAINT FK_CUPON_DESCUENTOXRECLAMO
@@ -477,7 +453,7 @@ FOREIGN KEY (ID_CUPON) REFERENCES MargeCreoQueOdioGDD.cupon_descuento
 
 ALTER TABLE MargeCreoQueOdioGDD.descuentoxpedido
 ADD CONSTRAINT FK_PEDIDO_DESCUENTOXPEDIDO
-FOREIGN KEY (ID_PEDIDO) REFERENCES MargeCreoQueOdioGDD.pedido
+FOREIGN KEY (ID_PEDIDO) REFERENCES MargeCreoQueOdioGDD.pedido --TODO: IDEM COMENTARIO ANTERIOR
 
 ALTER TABLE MargeCreoQueOdioGDD.descuentoxpedido
 ADD CONSTRAINT FK_CUPON_DESCUENTOXPEDIDO
@@ -515,35 +491,23 @@ ADD CONSTRAINT FK_MOVILIDAD_REPARTIDOR
 FOREIGN KEY (ID_TIPO_MOVILIDAD) REFERENCES MargeCreoQueOdioGDD.tipo_movilidad
 
 -------------------------- DIRECCION --------------------------
--------------------------- direccion --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.direccion
 ADD CONSTRAINT FK_LOCALIDAD_DIRECCION
 FOREIGN KEY (ID_LOCALIDAD) REFERENCES MargeCreoQueOdioGDD.localidad
 
--------------------------- localidad --------------------------
+-------------------------- LOCALIDAD --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.localidad
 ADD CONSTRAINT FK_PROVINCIA_LOCALIDAD
 FOREIGN KEY (ID_PROVINCIA) REFERENCES MargeCreoQueOdioGDD.provincia
 
 -------------------------- ENVIO MENSAJERIA --------------------------
--------------------------- estadoxmensajeria --------------------------
-ALTER TABLE MargeCreoQueOdioGDD.estadoxmensajeria
-ADD CONSTRAINT FK_MENSAJERIA_ESTADOXMENSAJERIA 
-FOREIGN KEY (ID_MENSAJERIA) REFERENCES MargeCreoQueOdioGDD.envio_mensajeria
-
-ALTER TABLE MargeCreoQueOdioGDD.estadoxmensajeria
-ADD CONSTRAINT FK_ESTADO_ESTADOXMENSAJERIA 
-FOREIGN KEY (ID_ESTADO) REFERENCES MargeCreoQueOdioGDD.estado_mensajeria
-
--------------------------- paquete --------------------------
-ALTER TABLE MargeCreoQueOdioGDD.paquete
-ADD CONSTRAINT FK_TIPO_PAQUETE_PAQUETE
-FOREIGN KEY (ID_TIPO_PAQUETE) REFERENCES MargeCreoQueOdioGDD.tipo_paquete
-
--------------------------- envio mensajeria --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.envio_mensajeria
 ADD CONSTRAINT FK_USUARIO_ENVIO_MENSAJERIA
 FOREIGN KEY (ID_USUARIO) REFERENCES MargeCreoQueOdioGDD.usuario
+
+ALTER TABLE MargeCreoQueOdioGDD.envio_mensajeria
+ADD CONSTRAINT FK_ESTADO_SERVICIO_MENSAJERIA
+FOREIGN KEY (ID_ESTADO) REFERENCES MargeCreoQueOdioGDD.estado_mensajeria
 
 ALTER TABLE MargeCreoQueOdioGDD.envio_mensajeria
 ADD CONSTRAINT FK_DIRECCION_ENVIO_MENSAJERIA
@@ -560,16 +524,23 @@ FOREIGN KEY (ID_PAQUETE) REFERENCES MargeCreoQueOdioGDD.paquete
 ALTER TABLE MargeCreoQueOdioGDD.envio_mensajeria
 ADD CONSTRAINT FK_ENVIO_ENVIO_MENSAJERIA
 FOREIGN KEY (ID_ENVIO) REFERENCES MargeCreoQueOdioGDD.envio
+------------------------------ Paquete -------------------------------
+ALTER TABLE MargeCreoQueOdioGDD.paquete
+ADD CONSTRAINT FK_TIPO_PAQUETE_PAQUETE
+FOREIGN KEY (ID_TIPO_PAQUETE) REFERENCES MargeCreoQueOdioGDD.tipo_paquete
 
 -------------------------- RECLAMO --------------------------
--------------------------- reclamo --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.reclamo
 ADD CONSTRAINT FK_USUARIO_RECLAMO 
 FOREIGN KEY (ID_USUARIO) REFERENCES MargeCreoQueOdioGDD.usuario;
 
 ALTER TABLE MargeCreoQueOdioGDD.reclamo
+ADD CONSTRAINT FK_ESTADO_RECLAMO
+FOREIGN KEY (ID_ESTADO) REFERENCES MargeCreoQueOdioGDD.estado_reclamo
+
+ALTER TABLE MargeCreoQueOdioGDD.reclamo
 ADD CONSTRAINT FK_PEDIDO_RECLAMO  
-FOREIGN KEY (ID_PEDIDO) REFERENCES MargeCreoQueOdioGDD.pedido;
+FOREIGN KEY (ID_PEDIDO) REFERENCES MargeCreoQueOdioGDD.pedido; -- TODO: IDEM ANTERIOR, REVISAR X TEMA DE DOBLE PK
 
 ALTER TABLE MargeCreoQueOdioGDD.reclamo
 ADD CONSTRAINT FK_OPERADOR_RECLAMO  
@@ -579,20 +550,77 @@ ALTER TABLE MargeCreoQueOdioGDD.reclamo
 ADD CONSTRAINT FK_TIPO_RECLAMO_RECLAMO  
 FOREIGN KEY (ID_TIPO_RECLAMO) REFERENCES MargeCreoQueOdioGDD.tipo_reclamo;
 
--------------------------- estadoxreclamo --------------------------
-ALTER TABLE MargeCreoQueOdioGDD.estadoxreclamo
-ADD CONSTRAINT FK_RECLAMO_ESTADOXRECLAMO
-FOREIGN KEY (ID_RECLAMO) REFERENCES MargeCreoQueOdioGDD.reclamo
-
-ALTER TABLE MargeCreoQueOdioGDD.estadoxreclamo
-ADD CONSTRAINT FK_ESTADO_ESTADOXRECLAMO
-FOREIGN KEY (ID_ESTADO) REFERENCES MargeCreoQueOdioGDD.estado_reclamo
-
--------------------------- operador --------------------------
+-------------------------- OPERADOR --------------------------
 ALTER TABLE MargeCreoQueOdioGDD.operador
 ADD CONSTRAINT FK_DIRECCION_OPERADOR
 FOREIGN KEY (ID_DIRECCION) REFERENCES MargeCreoQueOdioGDD.direccion
 
 /*********************** Creamos los stores procedures ***********************/
+CREATE TABLE MargeCreoQueOdioGDD.localidad (
+ID_LOCALIDAD INT IDENTITY(1,1), --PK
+NOMBRE NVARCHAR(255) NOT NULL,
+ID_PROVINCIA INT NOT NULL, --FK
+PRIMARY KEY (ID_LOCALIDAD)
+);
+---------------------------- Provincia ----------------------------
+CREATE PROCEDURE MargeCreoQueOdioGDD.migrar_provincias
+ AS
+  BEGIN
+	PRINT 'Migrando provincias...'
+    INSERT INTO provincia(NOMBRE)
+		SELECT Maestra.ENVIO_MENSAJERIA_PROVINCIA, Maestra.DIRECCION_USUARIO_PROVINCIA, Maestra.LOCAL_PROVINCIA
+		FROM gd_esquema.Maestra
+		-- TODO: Idem otros, al haber varios campos distintos provincia ver como se maneja
+  END
+GO
 
-/*********************** Realizamos la ejecución de los stores procedures ***********************/
+---------------------------- Localidad ----------------------------
+CREATE PROCEDURE MargeCreoQueOdioGDD.migrar_localidades
+ AS
+  BEGIN
+	PRINT 'Migrando localidades...'
+    INSERT INTO localidad (NOMBRE, ID_PROVINCIA)
+		SELECT Maestra.LOCAL_LOCALIDAD, Maestra.ENVIO_MENSAJERIA_LOCALIDAD, Maestra.DIRECCION_USUARIO_LOCALIDAD,
+		(SELECT provincia.ID_PROVINCIA
+		FROM provincia WHERE provincia.NOMBRE = Maestra.ENVIO_MENSAJERIA_PROVINCIA OR provincia.NOMBRE = Maestra.DIRECCION_USUARIO_PROVINCIA OR provincia.NOMBRE = Maestra.LOCAL_PROVINCIA)
+		-- TODO: Idem otros, al haber varios campos distintos provincia ver como se maneja
+		FROM gd_esquema.Maestra
+  END
+GO
+
+---------------------------- Direccion ----------------------------
+CREATE PROCEDURE MargeCreoQueOdioGDD.migrar_direcciones
+ AS
+  BEGIN
+	PRINT 'Migrando direcciones...'
+    INSERT INTO direccion (ID_LOCALIDAD, NOMBRE_DIRECCION, CALLE, NUMERO, CODIGO_POSTAL, DEPTO, OBSERVACIONES)
+		SELECT (SELECT localidad.ID_LOCALIDAD
+				FROM localidad 
+				WHERE localidad.NOMBRE = Maestra.DIRECCION_USUARIO_LOCALIDAD OR localidad.NOMBRE = Maestra.LOCAL_LOCALIDAD) AS Localidad
+				-- TODO: Tenemos distintos tipos de direcciones: usuario, operador, repartidor,... ver como migrar distintos campos
+				--CALLE = RTRIM(SUBSTRING(Maestra.DIRECCION_USUARIO_DIRECCION, 1, (LEN(Maestra.DIRECCION_USUARIO_DIRECCION) - 4)))
+				--NUMERO = LTRIM(RIGHT(Maestra.DIRECCION_USUARIO_DIRECCION, 4)))
+		FROM gd_esquema.Maestra
+FROM gd_esquema.Maestra
+  END
+GO
+
+---------------------------- Usuario ----------------------------
+CREATE PROCEDURE MargeCreoQueOdioGDD.migrar_usuarios
+ AS
+  BEGIN
+	PRINT 'Migrando usuarios...'
+    INSERT INTO usuario (NOMBRE, APELLIDO, DNI, FECHA_REGISTRO, TELEFONO, EMAIL, FECHA_NACIMIENTO, ID_DIRECCION)
+		SELECT Maestra.USUARIO_NOMBRE, USUARIO_APELLIDO, USUARIO_DNI, USUARIO_FECHA_REGISTRO, USUARIO_FECHA_REGISTRO,
+			   USUARIO_TELEFONO, USUARIO_MAIL, USUARIO_FECHA_NAC
+			   /*(SELECT direccion.ID_DIRECCION
+			   WHERE direccion.calle = RTRIM(SUBSTRING(DIRECCION_USUARIO_DIRECCION, 1, (LEN(DIRECCION_USUARIO_DIRECCION) - 4)))
+					 AND direccion.numero = LTRIM(RIGHT(DIRECCION_USUARIO_DIRECCION, 4))
+					 AND direccion.NOMBRE = Maestra.DIRECCION_USUARIO_NOMBRE
+					 -- TODO: codigo postal creo que no esta en la tabla Maestra
+			   )*/
+		FROM gd_esquema.Maestra
+  END
+GO
+
+/*********************** Realizamos la ejecuciÃ³n de los stores procedures ***********************/
